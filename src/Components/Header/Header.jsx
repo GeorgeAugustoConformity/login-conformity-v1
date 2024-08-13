7ca80c20db0f235e646a9ad15a98dad4e6f30444
void3333 <georgemiranda.sk@gmail.com>
2024-08-13 19:52:24 -0300

    added wrapper for header layout opt

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Header.css';
-import { useAuth } from '../../context/AuthContext';
+import {useAuth} from '../../context/AuthContext';
 import logo from '../../Components/Assets/header-logo.png';
 import logoBlue from '../../Components/Assets/logo-blue-new.png';
-import { MdOutlineLogout } from "react-icons/md";
+import {MdOutlineLogout} from "react-icons/md";
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
-import { useTheme } from '../../context/ThemeContext';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import {useTheme} from '../../context/ThemeContext';
+import {useNavigate} from 'react-router-dom'; // Import useNavigate
 
 const Header = () => {
-    const { theme } = useTheme(); // Access the current theme
-    const { logout } = useAuth();
+    const {theme} = useTheme(); // Access the current theme
+    const {logout} = useAuth();
     const navigate = useNavigate(); // Initialize useNavigate
 
     const handleLogout = () => {
@@ -21,14 +21,18 @@ const Header = () => {
 
     return (
         <header className="header">
-            {theme === "dark" ?
-                <img src={logo} alt="Logo" className="logo" /> :
-                <img src={logoBlue} alt="Logo" className="logo" />
-            }
-            <ThemeToggle className="icon"/>
-            <button className="logout-button" onClick={handleLogout}>
-                <MdOutlineLogout />
-            </button>
+            <div className="logo-wrapper">
+                {theme === "dark" ?
+                    <img src={logo} alt="Logo" className="logo"/> :
+                    <img src={logoBlue} alt="Logo" className="logo"/>
+                }
+            </div>
+            <div className="right-icons">
+                <ThemeToggle className="icon"/>
+                <button title="Sair" className="logout-button" onClick={handleLogout}>
+                    <MdOutlineLogout/>
+                </button>
+            </div>
         </header>
     );
 }
