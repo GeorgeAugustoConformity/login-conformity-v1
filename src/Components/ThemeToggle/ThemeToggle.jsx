fc34e057e45a455a637539c5687768f09e729e32
void3333 <georgemiranda.sk@gmail.com>
2024-08-13 23:03:17 -0300

    indent

--- src/Components/ThemeToggle/ThemeToggle.jsx
+++ src/Components/ThemeToggle/ThemeToggle.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './ThemeToggle.css';
-import { useTheme } from '../../context/ThemeContext';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import {useTheme} from '../../context/ThemeContext';
+import {FaSun, FaMoon} from 'react-icons/fa';
 
 const ThemeToggle = () => {
-    const { theme, toggleTheme } = useTheme();
+    const {theme, toggleTheme} = useTheme();
 
     return (
-        <button title="Mudar tema"  className="theme-toggle" onClick={toggleTheme}>
-            {theme === 'light' ? <FaMoon /> : <FaSun />}
+        <button title="Mudar tema" className="theme-toggle" onClick={toggleTheme}>
+            {theme === 'light' ? <FaMoon/> : <FaSun/>}
         </button>
     );
 }
