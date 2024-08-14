6cf7585fac295ade76c7df4ac5e072bbaf45b25f
void3333 <georgemiranda.sk@gmail.com>
2024-08-14 00:00:51 -0300

    clock and date added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
-import {useAuth} from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import logo from '../../Components/Assets/header-logo.png';
 import logoBlue from '../../Components/Assets/logo-blue-new.png';
-import {MdOutlineLogout} from "react-icons/md";
+import { MdOutlineLogout } from "react-icons/md";
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
-import {useTheme} from '../../context/ThemeContext';
-import {useNavigate} from 'react-router-dom'; // Import useNavigate
+import { useTheme } from '../../context/ThemeContext';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const Header = () => {
-    const {theme} = useTheme(); // Access the current theme
-    const {logout} = useAuth();
+    const { theme } = useTheme(); // Access the current theme
+    const { logout } = useAuth();
     const navigate = useNavigate(); // Initialize useNavigate
 
+    const [dateTime, setDateTime] = useState({
+        date: new Date().toLocaleDateString('en-GB'), // Date format DD-MM-YYYY
+        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) // Time without seconds
+    });
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            const now = new Date();
+            setDateTime({
+                date: now.toLocaleDateString('en-GB'),
+                time: now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) // Time without seconds
+            });
+        }, 1000);
+
+        // Clear the interval on component unmount
+        return () => clearInterval(timer);
+    }, []);
+
     const handleLogout = () => {
-        // console.log("Logout button clicked"); // Debugging
         logout(); // Perform logout
         navigate('/'); // Redirect to root path
     };
@@ -29,6 +46,10 @@ const Header = () => {
             </div>
             <div className="right-icons">
                 <ThemeToggle className="icon"/>
+                <div className="date-time">
+                    <span className="date">{dateTime.date}</span>
+                    <span className="time">{dateTime.time}</span>
+                </div>
                 <button title="Sair" className="logout-button" onClick={handleLogout}>
                     <MdOutlineLogout/>
                 </button>
