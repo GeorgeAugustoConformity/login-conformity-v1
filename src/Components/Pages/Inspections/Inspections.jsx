dcdd3390251b6c5fc7b5d74e810c2debe81e392b
void3333 <georgemiranda.sk@gmail.com>
2024-08-13 23:00:51 -0300

    clean spaces

---
+++ src/Components/Pages/Inspections/Inspections.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Inspections.css'
+
+const Inspections = () => {
+    return (
+        <div className="inspections">
+            <h2>Inspeções</h2>
+            <p>Chapecó é o destino!</p>
+        </div>
+    );
+};
+
+export default Inspections;
