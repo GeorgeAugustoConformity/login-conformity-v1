a3b3b324c6074bc43cf3e4187bb01a5b460bcf0f
void3333 <georgemiranda.sk@gmail.com>
2024-08-14 00:01:17 -0300

    prevent drag js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to prevent dragging
+    const preventDrag = (event) => {
+        event.preventDefault();
+    };
+
+    // Add event listeners to the entire document
+    document.addEventListener('dragstart', preventDrag, true);
+    document.addEventListener('drag', preventDrag, true);
+    document.addEventListener('dragend', preventDrag, true);
+
+    // Optionally, you can also lock text selection and image dragging
+    document.body.style.userSelect = 'none'; // Prevent text selection
+    document.body.style.webkitUserDrag = 'none'; // Prevent image dragging in WebKit browsers
+});
+
+    // Prevent context menu of opening
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
+
 root.render(
     <React.StrictMode>
         <App/>
