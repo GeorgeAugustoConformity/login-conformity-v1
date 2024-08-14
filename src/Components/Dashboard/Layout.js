836f72054649572a169d4ca7dcac26f0bd2180c2
void3333 <georgemiranda.sk@gmail.com>
2024-08-13 23:00:55 -0300

    clean spaces

---
+++ src/Components/Dashboard/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Header/Header';
+import SideMenu from '../SideMenu/SideMenu';
+import './Layout.css';
+
+const Layout = ({children}) => {
+    return (
+        <div className="dashboard">
+            <Header/>
+            <div className="main-content">
+                <SideMenu/>
+                <div className="content">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
