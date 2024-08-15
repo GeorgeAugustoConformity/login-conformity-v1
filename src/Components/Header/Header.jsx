71ffa41ff481880baafd4f16b26d4f69f8c37cf7
George <george.augusto@conformitycert.com>
2024-08-15 11:09:19 -0300

    dropdown added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import './Header.css';
-import { useAuth } from '../../context/AuthContext';
+import {useAuth} from '../../context/AuthContext';
 import logo from '../../Components/Assets/header-logo.png';
 import logoBlue from '../../Components/Assets/logo-blue-new.png';
-import { MdOutlineLogout } from "react-icons/md";
+import {MdOutlineLogout} from "react-icons/md";
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
-import { useTheme } from '../../context/ThemeContext';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import {useTheme} from '../../context/ThemeContext';
+import {useNavigate} from 'react-router-dom';
+import DropdownIcon from "../DropdownIcon/DropdownIcon"; // Import useNavigate
 
 const Header = () => {
-    const { theme } = useTheme(); // Access the current theme
-    const { logout } = useAuth();
+    const {theme} = useTheme(); // Access the current theme
+    const {logout} = useAuth();
     const navigate = useNavigate(); // Initialize useNavigate
 
     const [dateTime, setDateTime] = useState({
         date: new Date().toLocaleDateString('en-GB'), // Date format DD-MM-YYYY
-        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) // Time without seconds
+        time: new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) // Time without seconds
     });
 
     useEffect(() => {
@@ -23,7 +24,7 @@ const Header = () => {
             const now = new Date();
             setDateTime({
                 date: now.toLocaleDateString('en-GB'),
-                time: now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) // Time without seconds
+                time: now.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) // Time without seconds
             });
         }, 1000);
 
@@ -50,9 +51,10 @@ const Header = () => {
                     <span className="date">{dateTime.date}</span>
                     <span className="time">{dateTime.time}</span>
                 </div>
-                <button title="Sair" className="logout-button" onClick={handleLogout}>
-                    <MdOutlineLogout/>
-                </button>
+                {/*<button title="Sair" className="logout-button" onClick={handleLogout}>*/}
+                {/*    <MdOutlineLogout/>*/}
+                {/*</button>*/}
+                <DropdownIcon/>
             </div>
         </header>
     );
