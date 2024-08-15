2e3b4f2493aa24b9f48a199dc765f40121db7775
George <george.augusto@conformitycert.com>
2024-08-15 17:29:25 -0300

    added program (TODO)

--- src/Components/Sidemenu/SideMenu.jsx
+++ src/Components/Sidemenu/SideMenu.jsx
@@ -1,17 +1,14 @@
 import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import './SideMenu.css';
-import {RiArrowDownSLine, RiArrowUpSLine} from "react-icons/ri";
-// import {RiDashboardHorizontalFill, RiSurveyLine, RiArrowDownSLine, RiArrowUpSLine} from "react-icons/ri";
+import {RiArrowDownSLine, RiArrowUpSLine, RiCalendarScheduleLine} from "react-icons/ri";
 import {BiShowAlt} from "react-icons/bi";
 import {IoMdAddCircleOutline} from "react-icons/io";
-// import {HiOutlineDocumentReport} from "react-icons/hi";
-import {RiCalendarScheduleLine} from "react-icons/ri";
 import {GiSpy} from "react-icons/gi";
-import { GrCertificate } from "react-icons/gr";
-import { IoDocumentSharp } from "react-icons/io5";
-import { FaMoneyCheckDollar } from "react-icons/fa6";
-import { MdOutlineSupportAgent } from "react-icons/md";
+import {GrCertificate} from "react-icons/gr";
+import {IoDocumentSharp} from "react-icons/io5";
+import {FaMoneyCheckDollar} from "react-icons/fa6";
+import {MdOutlineSupportAgent} from "react-icons/md";
 
 const SideMenu = () => {
     const [expandedMenus, setExpandedMenus] = useState({});
@@ -25,19 +22,24 @@ const SideMenu = () => {
     return (<nav className="side-menu">
         <ul>
             <li>
+                {/*Programação*/}
                 <div className="expandable-menu">
                     <div onClick={() => toggleMenu('program')} className="expandable-menu-header">
-                        <RiCalendarScheduleLine className="icon-expandable"/><span className="span-title">Programação</span>
-                        {expandedMenus.program ? <RiArrowUpSLine className="arrow-icon"/> :
-                            <RiArrowDownSLine className="arrow-icon"/>}
+                        <RiCalendarScheduleLine
+                            className={`icon-expandable ${expandedMenus.program ? 'active' : ''}`}
+                        />
+                        <span className="span-title">Programação</span>
+                        {expandedMenus.program ? (<RiArrowUpSLine className="arrow-icon"/>) : (
+                            <RiArrowDownSLine className="arrow-icon"/>)}
                     </div>
                     {expandedMenus.program && (<ul className="expanded-menu">
                         <li>
                             <NavLink
-                                to="/dashboard/see-program"
+                                to="/dashboard/home"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <BiShowAlt className="icon"/><span>Ver Programações</span>
+                                <BiShowAlt className="icon"/>
+                                <span>Ver Programações</span>
                             </NavLink>
                         </li>
                         <li>
@@ -45,16 +47,22 @@ const SideMenu = () => {
                                 to="/dashboard/create-program"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoMdAddCircleOutline className="icon"/><span>Criar Programação</span>
+                                <IoMdAddCircleOutline className="icon"/>
+                                <span>Criar Programação</span>
                             </NavLink>
                         </li>
                     </ul>)}
                 </div>
+
+                {/*Inspeção*/}
                 <div className="expandable-menu">
                     <div onClick={() => toggleMenu('inspection')} className="expandable-menu-header">
-                        <GiSpy className="icon-expandable"/><span className="span-title">Inspeção</span>
-                        {expandedMenus.inspection ? <RiArrowUpSLine className="arrow-icon"/> :
-                            <RiArrowDownSLine className="arrow-icon"/>}
+                        <GiSpy
+                            className={`icon-expandable ${expandedMenus.inspection ? 'active' : ''}`}
+                        />
+                        <span className="span-title">Inspeção</span>
+                        {expandedMenus.inspection ? (<RiArrowUpSLine className="arrow-icon"/>) : (
+                            <RiArrowDownSLine className="arrow-icon"/>)}
                     </div>
                     {expandedMenus.inspection && (<ul className="expanded-menu">
                         <li>
@@ -62,7 +70,8 @@ const SideMenu = () => {
                                 to="/dashboard/see-inspection-data"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <BiShowAlt className="icon"/><span>Ver Inspeções</span>
+                                <BiShowAlt className="icon"/>
+                                <span>Ver Inspeções</span>
                             </NavLink>
                         </li>
                         <li>
@@ -70,16 +79,22 @@ const SideMenu = () => {
                                 to="/dashboard/see-inspection-process"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoMdAddCircleOutline className="icon"/><span>Enviar Fotos</span>
+                                <IoMdAddCircleOutline className="icon"/>
+                                <span>Enviar Fotos</span>
                             </NavLink>
                         </li>
                     </ul>)}
                 </div>
+
+                {/*Certificação*/}
                 <div className="expandable-menu">
                     <div onClick={() => toggleMenu('certification')} className="expandable-menu-header">
-                        <GrCertificate className="icon-expandable"/><span className="span-title">Certificação</span>
-                        {expandedMenus.certification ? <RiArrowUpSLine className="arrow-icon"/> :
-                            <RiArrowDownSLine className="arrow-icon"/>}
+                        <GrCertificate
+                            className={`icon-expandable ${expandedMenus.certification ? 'active' : ''}`}
+                        />
+                        <span className="span-title">Certificação</span>
+                        {expandedMenus.certification ? (<RiArrowUpSLine className="arrow-icon"/>) : (
+                            <RiArrowDownSLine className="arrow-icon"/>)}
                     </div>
                     {expandedMenus.certification && (<ul className="expanded-menu">
                         <li>
@@ -87,7 +102,8 @@ const SideMenu = () => {
                                 to="/dashboard/see-certification-data"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <BiShowAlt className="icon"/><span>Ver Certificados</span>
+                                <BiShowAlt className="icon"/>
+                                <span>Ver Certificados</span>
                             </NavLink>
                         </li>
                         <li>
@@ -95,16 +111,22 @@ const SideMenu = () => {
                                 to="/dashboard/see-certification-process"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoMdAddCircleOutline className="icon"/><span>Gerar Certificado</span>
+                                <IoMdAddCircleOutline className="icon"/>
+                                <span>Gerar Certificado</span>
                             </NavLink>
                         </li>
                     </ul>)}
                 </div>
+
+                {/*Documentação*/}
                 <div className="expandable-menu">
                     <div onClick={() => toggleMenu('documentation')} className="expandable-menu-header">
-                        <IoDocumentSharp className="icon-expandable"/><span className="span-title">Documentação</span>
-                        {expandedMenus.documentation ? <RiArrowUpSLine className="arrow-icon"/> :
-                            <RiArrowDownSLine className="arrow-icon"/>}
+                        <IoDocumentSharp
+                            className={`icon-expandable ${expandedMenus.documentation ? 'active' : ''}`}
+                        />
+                        <span className="span-title">Documentação</span>
+                        {expandedMenus.documentation ? (<RiArrowUpSLine className="arrow-icon"/>) : (
+                            <RiArrowDownSLine className="arrow-icon"/>)}
                     </div>
                     {expandedMenus.documentation && (<ul className="expanded-menu">
                         <li>
@@ -112,7 +134,8 @@ const SideMenu = () => {
                                 to="/dashboard/see-documentation-data"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <BiShowAlt className="icon"/><span>Ver Certificados</span>
+                                <BiShowAlt className="icon"/>
+                                <span>Ver Documentação</span>
                             </NavLink>
                         </li>
                         <li>
@@ -120,16 +143,22 @@ const SideMenu = () => {
                                 to="/dashboard/see-documentation-process"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoMdAddCircleOutline className="icon"/><span>Gerar Certificado</span>
+                                <IoMdAddCircleOutline className="icon"/>
+                                <span>Gerar Documentação</span>
                             </NavLink>
                         </li>
                     </ul>)}
                 </div>
+
+                {/*Financeiro*/}
                 <div className="expandable-menu">
                     <div onClick={() => toggleMenu('finance')} className="expandable-menu-header">
-                        <FaMoneyCheckDollar className="icon-expandable"/><span className="span-title">Financeiro</span>
-                        {expandedMenus.finance ? <RiArrowUpSLine className="arrow-icon"/> :
-                            <RiArrowDownSLine className="arrow-icon"/>}
+                        <FaMoneyCheckDollar
+                            className={`icon-expandable ${expandedMenus.finance ? 'active' : ''}`}
+                        />
+                        <span className="span-title">Financeiro</span>
+                        {expandedMenus.finance ? (<RiArrowUpSLine className="arrow-icon"/>) : (
+                            <RiArrowDownSLine className="arrow-icon"/>)}
                     </div>
                     {expandedMenus.finance && (<ul className="expanded-menu">
                         <li>
@@ -137,7 +166,8 @@ const SideMenu = () => {
                                 to="/dashboard/see-finance-data"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <BiShowAlt className="icon"/><span>Ver Certificados</span>
+                                <BiShowAlt className="icon"/>
+                                <span>Ver Dados Financeiros</span>
                             </NavLink>
                         </li>
                         <li>
@@ -145,16 +175,22 @@ const SideMenu = () => {
                                 to="/dashboard/see-finance-process"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoMdAddCircleOutline className="icon"/><span>Gerar Certificado</span>
+                                <IoMdAddCircleOutline className="icon"/>
+                                <span>Gerar Relatório Financeiro</span>
                             </NavLink>
                         </li>
                     </ul>)}
                 </div>
+
+                {/*Suporte*/}
                 <div className="expandable-menu">
                     <div onClick={() => toggleMenu('support')} className="expandable-menu-header">
-                        <MdOutlineSupportAgent className="icon-expandable"/><span className="span-title">Suporte</span>
-                        {expandedMenus.support ? <RiArrowUpSLine className="arrow-icon"/> :
-                            <RiArrowDownSLine className="arrow-icon"/>}
+                        <MdOutlineSupportAgent
+                            className={`icon-expandable ${expandedMenus.support ? 'active' : ''}`}
+                        />
+                        <span className="span-title">Suporte</span>
+                        {expandedMenus.support ? (<RiArrowUpSLine className="arrow-icon"/>) : (
+                            <RiArrowDownSLine className="arrow-icon"/>)}
                     </div>
                     {expandedMenus.support && (<ul className="expanded-menu">
                         <li>
@@ -162,7 +198,8 @@ const SideMenu = () => {
                                 to="/dashboard/see-support-data"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <BiShowAlt className="icon"/><span>Ver Certificados</span>
+                                <BiShowAlt className="icon"/>
+                                <span>Ver Dados de Suporte</span>
                             </NavLink>
                         </li>
                         <li>
@@ -170,7 +207,8 @@ const SideMenu = () => {
                                 to="/dashboard/see-support-process"
                                 className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoMdAddCircleOutline className="icon"/><span>Gerar </span>
+                                <IoMdAddCircleOutline className="icon"/>
+                                <span>Gerar Relatório de Suporte</span>
                             </NavLink>
                         </li>
                     </ul>)}
