9cd686d844aa604b87633bb92e1f533aaf1b5bb4
George <george.augusto@conformitycert.com>
2024-08-15 17:28:04 -0300

    indent

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="footer-content">
                 {/* Other footer content */}
             </div>
-            <NewsTicker />
+            <NewsTicker/>
         </footer>
     );
 };
