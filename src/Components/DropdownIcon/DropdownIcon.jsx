62c13637aa07a478b1c7efce8fb8546f9ba5673a
George <george.augusto@conformitycert.com>
2024-08-15 17:27:53 -0300

    indent

--- src/Components/DropdownIcon/DropdownIcon.jsx
+++ src/Components/DropdownIcon/DropdownIcon.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
-import { IoIosSettings } from 'react-icons/io';
-import { CgProfile } from 'react-icons/cg';
-import { MdOutlineLogout } from 'react-icons/md';
-import { useAuth } from '../../context/AuthContext';
+import React, {useState, useEffect, useRef} from 'react';
+import {NavLink, useNavigate} from 'react-router-dom';
+import {FaUserCircle} from 'react-icons/fa';
+import {IoIosSettings} from 'react-icons/io';
+import {CgProfile} from 'react-icons/cg';
+import {MdOutlineLogout} from 'react-icons/md';
+import {useAuth} from '../../context/AuthContext';
 import './DropdownIcon.css';
 
 const DropdownIcon = () => {
@@ -15,7 +15,7 @@ const DropdownIcon = () => {
         setIsOpen(!isOpen);
     };
 
-    const { logout } = useAuth();
+    const {logout} = useAuth();
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -38,29 +38,29 @@ const DropdownIcon = () => {
 
     return (
         <div className="dropdown-icon" ref={dropdownRef}>
-            <FaUserCircle title="Minha Conta" onClick={toggleDropdown} className="icon" />
+            <FaUserCircle title="Minha Conta" onClick={toggleDropdown} className="icon"/>
             {isOpen && (
                 <div className="dropdown-menu">
                     <ul>
                         <li>
                             <NavLink
                                 to="/dashboard/profile"
-                                className={({ isActive }) => (isActive ? 'active' : '')}
+                                className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <CgProfile title="Perfil" className="icon" />
+                                <CgProfile title="Perfil" className="icon"/>
                             </NavLink>
                         </li>
                         <li>
                             <NavLink
                                 to="/dashboard/settings"
-                                className={({ isActive }) => (isActive ? 'active' : '')}
+                                className={({isActive}) => (isActive ? 'active' : '')}
                             >
-                                <IoIosSettings title="Configurações" className="icon" />
+                                <IoIosSettings title="Configurações" className="icon"/>
                             </NavLink>
                         </li>
                         <li>
                             <button title="Sair" className="logout-button" onClick={handleLogout}>
-                                <MdOutlineLogout />
+                                <MdOutlineLogout/>
                             </button>
                         </li>
                     </ul>
