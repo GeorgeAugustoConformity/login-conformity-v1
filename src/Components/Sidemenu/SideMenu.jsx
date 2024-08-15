9ceb7d94fcfe248d4018da983932556848e992e6
George <george.augusto@conformitycert.com>
2024-08-15 11:12:03 -0300

    expandable added

--- src/Components/Sidemenu/SideMenu.jsx
+++ src/Components/Sidemenu/SideMenu.jsx
@@ -1,55 +1,81 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './SideMenu.css';
-import {RiDashboardHorizontalFill} from "react-icons/ri";
-import {CgProfile} from "react-icons/cg";
-import {IoIosSettings} from "react-icons/io";
-import {HiOutlineDocumentReport} from "react-icons/hi";
-import {RiSurveyLine} from "react-icons/ri";
+import { RiDashboardHorizontalFill, RiSurveyLine, RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import { BiShowAlt } from "react-icons/bi";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiOutlineDocumentReport } from "react-icons/hi";
+import { RiCalendarScheduleLine } from "react-icons/ri";
+import { GiSpy } from "react-icons/gi";
 
 const SideMenu = () => {
-     return (
+    const [expandedMenus, setExpandedMenus] = useState({});
+
+    const toggleMenu = (menuName) => {
+        setExpandedMenus((prev) => ({
+            ...prev,
+            [menuName]: !prev[menuName]
+        }));
+    };
+
+    return (
         <nav className="side-menu">
             <ul>
                 <li>
-                    <NavLink
-                        to="/dashboard/home"
-                        className={({isActive}) => isActive ? 'active' : ''}
-                    >
-                        <RiDashboardHorizontalFill className="icon"/>Dashboard
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/dashboard/profile"
-                        className={({isActive}) => isActive ? 'active' : ''}
-                    >
-                        <CgProfile className="icon"/>Perfil
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/dashboard/settings"
-                        className={({isActive}) => isActive ? 'active' : ''}
-                    >
-                        <IoIosSettings className="icon"/>Configurações
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/dashboard/reports"
-                        className={({isActive}) => isActive ? 'active' : ''}
-                    >
-                        <HiOutlineDocumentReport className="icon"/>Relatórios
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/dashboard/inspections"
-                        className={({isActive}) => isActive ? 'active' : ''}
-                    >
-                        <RiSurveyLine className="icon"/>Inspeções
-                    </NavLink>
+                    <div className="expandable-menu">
+                        <div onClick={() => toggleMenu('program')} className="expandable-menu-header">
+                            <RiCalendarScheduleLine/>Programação
+                            {expandedMenus.program ? <RiArrowUpSLine className="arrow-icon"/> :
+                                <RiArrowDownSLine className="arrow-icon"/>}
+                        </div>
+                        {expandedMenus.program && (
+                            <ul className="expanded-menu">
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/see-program"
+                                        className={({ isActive }) => (isActive ? 'active' : '')}
+                                    >
+                                        <BiShowAlt className="icon"/>Ver Programações
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/create-program"
+                                        className={({ isActive }) => (isActive ? 'active' : '')}
+                                    >
+                                        <IoMdAddCircleOutline className="icon"/>Criar Programação
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        )}
+                    </div>
+                    <div className="expandable-menu">
+                        <div onClick={() => toggleMenu('inspection')} className="expandable-menu-header">
+                            <GiSpy/>Inspeção
+                            {expandedMenus.inspection ? <RiArrowUpSLine className="arrow-icon"/> :
+                                <RiArrowDownSLine className="arrow-icon"/>}
+                        </div>
+                        {expandedMenus.inspection && (
+                            <ul className="expanded-menu">
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/see-inspection-data"
+                                        className={({ isActive }) => (isActive ? 'active' : '')}
+                                    >
+                                        <BiShowAlt className="icon"/>Ver Inspeções
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/see-inspection-process"
+                                        className={({ isActive }) => (isActive ? 'active' : '')}
+                                    >
+                                        <IoMdAddCircleOutline className="icon"/>Enviar Fotos
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        )}
+                    </div>
                 </li>
             </ul>
         </nav>
